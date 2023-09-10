d18a34c4c6964a88f348c0904fe618a2d1d2b702
Ade3164 <Adebayoidris051@gmail.com>
2023-09-10 12:37:39 +0100

    hng internship stage1

--- app.py
+++ app.py
@@ -1,46 +1,33 @@
-from flask import Flask, request, jsonify
-import datetime
-import pytz
+#!flask/bin/python
+from flask import Flask, jsonify, request
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-  return "Welcome!"
+  return "Welcome! kindly use th api service at /api"
 
 @app.route('/api', methods=['GET'])
 def get_item():
+  """Retrieves all data of the item with the slack_name and track param"""
+  slack_name = request.args.get('slack_name')
+  current_day = datetime.now().strftime('%A')
+  utc_time = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+  track = request.args.get('track')
+  github_file_url = "https://github.com/Ade3164/endpoint/blob/main/app.py "
+  github_repo_url = "https://github.com/Ade3164/endpoint"
     
-    slack_name = request.args.get('slack_name')
-    track = request.args.get('track')
-
-    current_day = datetime.datetime.now(pytz.utc).astimezone(pytz.timezone('UTC')).strftime('%A')
-
-
-    current_utc_time = datetime.datetime.now(pytz.utc).astimezone(pytz.timezone('UTC'))
-    min_time = current_utc_time - datetime.timedelta(minutes=2)
-    max_time = current_utc_time + datetime.timedelta(minutes=2)
-
-    github_file_url = "https://github.com/Ade3164/endpoint/blob/main/app.py "
-    github_repo_url = "https://github.com/Ade3164/endpoint"
-    status_code = 200
-
-  
-    response_data = {
-        "slack_name": slack_name,
-        "current_day": current_day,
-        "utc_time": current_utc_time.strftime('%Y-%m-%dT%H:%M:%SZ'),
-        "track": track,
-        "github_file_url": github_file_url,
-        "github_repo_url": github_repo_url,
-        "status_code": status_code
-    }
-
-    # Set the response content type to JSON
-    response = jsonify(response_data)
-
-    return response
+  res = {
+     "slack_name": slack_name,
+     "current_day": current_day,
+     "utc_time": utc_time,
+     "track": track,
+     "github_file_url": github_file_url,
+     "github_repo_url": github_repo_url,
+     "status_code": 200,
+  }
+  return jsonify(res)
 
 if __name__ == '__main__':
-  
     app.run(debug=True)
