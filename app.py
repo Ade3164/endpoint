9f4cd64036ff6076bb5cf015f6508f6bb7a5b41f
Ade3164 <Adebayoidris051@gmail.com>
2023-09-10 12:54:38 +0100

    HNG

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-  return "Welcome! kindly use th api service at /api"
+  return "Welcome /api"
 
 @app.route('/api', methods=['GET'])
 def get_item():
